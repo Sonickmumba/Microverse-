9164b2b6d721f9706b3f6d4d320a7d2fe5d48ae0
sonickmumba <kingellie.mumba@gmail.com>
2022-05-24 23:11:35 +0200

    added service.py

---
+++ score.py
@@ -0,0 +1,14 @@
+score = [10,5,20,20,4,5,2]
+
+highest = score[0]
+lowest = score[0]
+
+for i in range(1, len(score) -1):
+    if score[i] > highest:
+        highest = score[i]
+        if score[i] < lowest:
+            lowest = score[i]
+
+
+print(highest)
+print(lowest)
